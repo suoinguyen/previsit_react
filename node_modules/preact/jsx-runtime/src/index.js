9edadc9800769a44a9648c8a2fc05c3abf758cb3
Tainv <tainv@its-global.vn>
2024-12-04 18:39:51 +0700

    update

--- node_modules/preact/jsx-runtime/src/index.js
+++ node_modules/preact/jsx-runtime/src/index.js
@@ -35,15 +35,9 @@ function createVNode(type, props, key, isStaticChildren, __source, __self) {
 		ref,
 		i;
 
-	if ('ref' in normalizedProps) {
-		normalizedProps = {};
-		for (i in props) {
-			if (i == 'ref') {
-				ref = props[i];
-			} else {
-				normalizedProps[i] = props[i];
-			}
-		}
+	if ('ref' in props) {
+		ref = props.ref;
+		delete props.ref;
 	}
 
 	/** @type {VNode & { __source: any; __self: any }} */
@@ -56,7 +50,6 @@ function createVNode(type, props, key, isStaticChildren, __source, __self) {
 		_parent: null,
 		_depth: 0,
 		_dom: null,
-		_nextDom: undefined,
 		_component: null,
 		constructor: undefined,
 		_original: --vnodeId,
